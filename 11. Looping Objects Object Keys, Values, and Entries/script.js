1c61245859293161fe57f979117a55b9ba60d3c5
ephraim <ephraims.818@gmail.com>
2025-03-11 02:55:48 +0600

    JS--DATA-STRUCTURES-MODERN-OPERATORS-AND-STRINGS - 11. Looping Objects Object Keys, Values, and Entries

---
+++ 11. Looping Objects Object Keys, Values, and Entries/script.js
@@ -0,0 +1,84 @@
+'use strict';
+
+// Data needed for first part of the section
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  // Enhanced Object Literal
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: 'Cla1ssico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  // Enhanced Object Literals
+  openingHours,
+
+  // Enhanced Object Literals
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]}, ${this.mainMenu[mainIndex]} will be delivered to ${address}, at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+//  Looping Objects with for...of Loop
+/* NOTE: In JavaScript, the for...of loop is generally used for iterating over iterable objects like arrays, strings, and other collections. However, objects themselves are not directly iterable, so you can't directly use for...of to loop over an object.
+
+But we can work around this limitation by using Object.entries(), Object.keys(), or Object.values() to convert the object properties into iterable arrays.
+ */
+
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+const values = Object.values(openingHours);
+console.log(values);
+
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
