2bc5a11cedeb7da179f513c77cfc9b56fdc6f836
ephraim <ephraims.818@gmail.com>
2025-03-10 16:01:13 +0600

    JS--DATA-STRUCTURES-MODERN-OPERATORS-AND-STRINGS - 08. Looping Arrays The for of Loop

---
+++ 08. Looping Arrays The for of Loop/script.js
@@ -0,0 +1,68 @@
+'use strict';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Cla1ssico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]}, ${this.mainMenu[mainIndex]} will be delivered to ${address}, at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+// The for...of Loop
+/* NOTE: The for...of loop is a modern loop introduced in ES6 (ECMAScript 2015) that allows iteration over iterable objects, such as arrays, strings, maps, sets, NodeLists, and more. It provides a cleaner and more readable syntax compared to traditional loops like for and forEach().
+ */
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+for (const item of menu) {
+  console.log(item);
+}
+
+/* for (const item of menu.entries()) {
+  console.log(item);
+} */
+
+for (const [index, element] of menu.entries()) {
+  console.log(`${index + 1}. ${element}`);
+}
