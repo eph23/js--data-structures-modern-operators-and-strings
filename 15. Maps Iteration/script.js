32b0e2885fbe3acee86d6c7aece203140d4c4146
ephraim <ephraims.818@gmail.com>
2025-03-11 20:55:42 +0600

    JS--DATA-STRUCTURES-MODERN-OPERATORS-AND-STRINGS - 15. Maps Iteration

---
+++ 15. Maps Iteration/script.js
@@ -0,0 +1,88 @@
+'use strict';
+
+// Data needed for first part of the section
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  // Enhanced Object Literal
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: 'Cla1ssico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  // Enhanced Object Literals
+  openingHours,
+
+  // Enhanced Object Literals
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]}, ${this.mainMenu[mainIndex]} will be delivered to ${address}, at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+//  Maps Iteration
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct'],
+  [false, 'Try again'],
+]);
+console.log(question);
+
+//Quiz app
+console.log(question.get('question'));
+for (const [i, q] of question) {
+  if (typeof i === 'number') {
+    console.log(`Answer ${i}: ${q}`);
+  }
+}
+
+// const answer = Number(prompt('Your answer '));
+// console.log(answer);
+// console.log(question.get(question.get('correct') === answer));
+
+// Converting map to array
+console.log(...question);
+console.log(...question.entries());
+console.log(...question.keys());
+console.log(...question.values());
